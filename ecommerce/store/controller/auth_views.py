96e69dd7ce5dfc925193199e5e558839f9b78f7c
Sajjad Ali <sajjadali.dev01@gamil.com>
2026-05-10 20:03:26 +0530

    feat: For login and For Logout functionality

--- ecommerce/store/controller/auth_views.py
+++ ecommerce/store/controller/auth_views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from store.forms import CustomUserForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout  # for login
 
 # Create your Login, Logout / views here 👇.
 
-# Registration
+# For Registration
 def register_view(request):
     form = CustomUserForm()
     if request.method == 'POST':
@@ -17,7 +18,36 @@ def register_view(request):
     context = {'form':form}
     return render(request, 'store/auth/register.html', context)
 
-# Login 
+# For Login 
 def login_page_view(request):
-    return render(request, 'store/auth/login.html')
 
+    if request.user.is_authenticated:  # If u r login try to do --> http://localhost:8000/login/ so hey will not accept 
+        messages.warning(request, "You are already loged in")
+        return redirect("/") 
+    else:
+
+        if request.method == 'POST':
+            name = request.POST.get('username')  # login.html --> name="username"
+            pwd = request.POST.get('password') # login.html --> name="password"
+
+            user = authenticate(request, username=name, password=pwd)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Loged In Sucessfully")
+                return redirect('/')
+            
+            else:
+                messages.error(request, "Invalid Username or Passwoprd")
+                return redirect('/login')
+
+
+        return render(request, 'store/auth/login.html')
+
+
+# For Logout
+def logout_page_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "Logged out Sucessfully!")
+        return redirect('/')
