3dad1edb25e9f5051831bc84f4fe650a91e528a4
Sajjad Ali <yoursajju61@gmail.com>
2026-05-18 14:42:44 +0530

    fix: Add to Cart Alertify popup text

--- ecommerce/static/js/custom.js
+++ ecommerce/static/js/custom.js
@@ -34,22 +34,27 @@ $(document).ready(function () {
 
         let product_id = $(this).closest('.product_data').find('.prod_id').val();
         let product_qty = $(this).closest('.product_data').find('.qty-input').val();
-        let token  = $('input[name=csrfmiddlewaretoken]').val();
+        let token = $('input[name=csrfmiddlewaretoken]').val();
 
         $.ajax({
             method: "POST",
             url: "/add-to-cart/",
             data: {
-                'product_id':product_id,
-                'product_qty':product_qty,
+                'product_id': product_id,
+                'product_qty': product_qty,
                 csrfmiddlewaretoken: token
 
             },
-            
-            success: function (response) {
-                console.log(response)
-                alertify.success(response.status)
 
+            success: function (response) {
+                console.log(response);
+
+                if (response.error) {
+                    alertify.error(response.error);
+                }
+                else if (response.status) {
+                    alertify.success(response.status);
+                }
             }
 
         });
