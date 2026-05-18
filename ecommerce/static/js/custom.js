7a3ec678d4e9e18688c5315b6739cce845a2b72a
Sajjad Ali <sajjadali.dev01@gamil.com>
2026-05-18 13:10:26 +0530

    feat:In ADD TO CART  Add addToCartBtn fun in custom.js

--- ecommerce/static/js/custom.js
+++ ecommerce/static/js/custom.js
@@ -28,4 +28,34 @@ $(document).ready(function () {
 
     });
 
-})
+
+    $('.addToCartBtn').click(function (e) {
+        e.preventDefault();
+
+        let product_id = $(this).closest('.product_data').find('.prod_id').val();
+        let product_qty = $(this).closest('.product_data').find('.qty-input').val();
+        let token  = $('input[name=csrfmiddlewaretoken]').val();
+
+        $.ajax({
+            method: "POST",
+            url: "/add-to-cart/",
+            data: {
+                'product_id':product_id,
+                'product_qty':product_qty,
+                csrfmiddlewaretoken: token
+
+            },
+            
+            success: function (response) {
+                console.log(response)
+                alertify.success(response.status)
+
+            }
+
+        });
+
+    });
+
+
+
+});
