587bab796257768ccff4bee03b75f5006ce3c88d
Sajjad Ali <yoursajju61@gmail.com>
2026-06-08 18:20:27 +0530

    feat: Add Delete functionality

--- ecommerce/static/js/custom.js
+++ ecommerce/static/js/custom.js
@@ -59,6 +59,97 @@ $(document).ready(function () {
 
         });
 
+
+
+
+    });
+    $('.changeQuantity').click(function (e) {
+        e.preventDefault();
+
+        let product_id = $(this).closest('.product_data').find('.prod_id').val();
+        let product_qty = $(this).closest('.product_data').find('.qty-input').val();
+        let token = $('input[name=csrfmiddlewaretoken]').val();
+
+        $.ajax({
+            method: "POST",
+            url: "/update-cart/",
+            data: {
+                'product_id': product_id,
+                'product_qty': product_qty,
+                csrfmiddlewaretoken: token
+
+            },
+
+            success: function (response) {
+                console.log(response);
+
+                if (response.error) {
+                    alertify.error(response.error);
+                }
+                else if (response.status) {
+                    alertify.success(response.status);
+                }
+            }
+
+        });
+
+    });
+
+    // Delete Cart Item
+    // $('.delete-cart-item').click(function (e) {
+    //     e.preventDefault();
+    //     // Fetch:- product id,
+    //     let product_id = $(this).closest('.product_data').find('.prod_id').val();
+    //     let token = $('input[name=csrfmiddlewaretoken]').val();
+
+    //     $.ajax({
+    //         method: "POST",
+    //         url: "delete-cart-item",
+    //         data: {
+    //             'product_id': product_id,
+    //             csrfmiddlewaretoken: token
+    //         },
+    //         success: function (response) {
+    //             alertify.sucess(response.status);
+    //             $('.cartdata').load(location.href + ' .cartdata');
+
+    //         }
+
+    //     });
+
+
+
+    // });
+
+    $(document).ready(function () {
+        // Event Delegation use kiya hai taaki dynamic reload (.load) ke baad bhi button kaam kare
+        $(document).on('click', '.delete-cart-item', function (e) {
+            e.preventDefault();
+
+            let $this = $(this); // Current button ka reference
+            let product_id = $this.closest('.product_data').find('.prod_id').val();
+            let token = $('input[name=csrfmiddlewaretoken]').val();
+
+            $.ajax({
+                method: "POST",
+                url: "/delete-cart-item/", // URL ke aage aur peeche slash (/) zaroor lagayein
+                data: {
+                    'product_id': product_id,
+                    csrfmiddlewaretoken: token
+                },
+                success: function (response) {
+                    // Sahi spelling 'success' ke sath Alertify notification
+                    alertify.success(response.status);
+
+                    // Sirf cart data wale part ko refresh karne ke liye
+                    $('.cartdata').load(location.href + ' .cartdata');
+                },
+                error: function (xhr, status, error) {
+                    console.log("Error details:", error);
+                    alertify.error("Something went wrong! Please try again.");
+                }
+            });
+        });
     });
 
 
