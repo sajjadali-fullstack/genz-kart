0bc8261129ecc55a887fbf138423014658d5ad9f
Sajjad Ali <yoursajju61@gmail.com>
2026-06-14 16:37:28 +0530

    feat: Create Wishlist model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -60,3 +60,13 @@ class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     product_qty = models.IntegerField(null=False, blank=False)
     create_at = models.DateTimeField(auto_now_add=True)
+
+
+# Wishlist View
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
