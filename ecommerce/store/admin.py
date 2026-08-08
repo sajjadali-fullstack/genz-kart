8e81019c4d888c01ab1db36cd55dfb37a5d90140
Sajjad Ali <yoursajju61@gmail.com>
2026-08-09 01:26:28 +0530

    Register order and profile models in admin

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Category, Product, Cart
+from store.models import Category, Product, Cart, Order, OrderItem, Profile
 
 
 # Register your models here.
@@ -7,3 +7,6 @@ from store.models import Category, Product, Cart
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem) 
+admin.site.register(Profile)
