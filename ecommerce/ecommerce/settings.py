515b2fc8ac68343018c3c0e74eb97caf6333bc8d
Sajjad Ali <yoursajju61@gmail.com>
2026-08-09 16:14:42 +0530

    Configure Allowed Host

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'genzkart.pythonanywhere.com',
+]
 
 
 # Application definition
